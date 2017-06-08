0da99ceee64f767debdcd1d565062507f6c1fbad
胡萌 <humengstark@humengdeMacBook-Pro.local>
2017-06-08 22:34:02 +0800

    Initial Commit

---
+++ studyObjective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  studyObjective-C
+//
+//  Created by 胡萌 on 2017/6/8.
+//  Copyright © 2017年 humengstark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
